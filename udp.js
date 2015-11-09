238f1da6d86a02f0a9b2243f0d080534c14b321a
Thomas Watson Steen <w@tson.dk>
2015-11-09 11:44:32 +0100

    Add discover support

--- udp.js
+++ udp.js
@@ -1,7 +1,9 @@
 'use strict'
 
 var publish = require('./lib/registry').publish
+var discover = require('./lib/discover')
 
 var PROTOCOL = 'udp'
 
 exports.publish = publish.bind(null, PROTOCOL)
+exports.discover = discover.bind(null, PROTOCOL)
