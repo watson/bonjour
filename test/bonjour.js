c5aa5ae240b19f78bc5b15f85b7c3627f706956d
Thomas Watson Steen <w@tson.dk>
2015-12-27 11:56:12 +0100

    Add overall tests

---
+++ test/bonjour.js
@@ -0,0 +1,116 @@
+'use strict'
+
+var os = require('os')
+var dgram = require('dgram')
+var tape = require('tape')
+var afterAll = require('after-all')
+var Service = require('../lib/service')
+var Bonjour = require('../')
+
+var port = function (cb) {
+  var s = dgram.createSocket('udp4')
+  s.bind(0, function () {
+    var port = s.address().port
+    s.on('close', function () {
+      cb(port)
+    })
+    s.close()
+  })
+}
+
+var test = function (name, fn) {
+  tape(name, function (t) {
+    port(function (p) {
+      fn(Bonjour({ ip: '127.0.0.1', port: p, multicast: false }), t)
+    })
+  })
+}
+
+test('bonjour.publish', function (bonjour, t) {
+  var service = bonjour.publish({ name: 'foo', type: 'bar', port: 3000 })
+  t.ok(service instanceof Service)
+  t.equal(service.published, false)
+  service.on('up', function () {
+    t.equal(service.published, true)
+    bonjour.destroy()
+    t.end()
+  })
+})
+
+test('bonjour.unpublishAll', function (bonjour, t) {
+  var service = bonjour.publish({ name: 'foo', type: 'bar', port: 3000 })
+  service.on('up', function () {
+    bonjour.unpublishAll(function () {
+      t.equal(service.published, false)
+      bonjour.destroy()
+      t.end()
+    })
+  })
+})
+
+test('bonjour.find', function (bonjour, t) {
+  var next = afterAll(function () {
+    var browser = bonjour.find({ type: 'test' })
+    var ups = 0
+
+    browser.on('up', function (s) {
+      if (s.name === 'Foo Bar') {
+        t.equal(s.name, 'Foo Bar')
+        t.equal(s.fqdn, 'Foo Bar._test._tcp.local')
+        t.deepEqual(s.txt, {})
+        t.deepEqual(s.rawTxt, new Buffer('00', 'hex'))
+      } else {
+        t.equal(s.name, 'Baz')
+        t.equal(s.fqdn, 'Baz._test._tcp.local')
+        t.deepEqual(s.txt, { foo: new Buffer('bar') })
+        t.deepEqual(s.rawTxt, new Buffer('07666f6f3d626172', 'hex'))
+      }
+      t.equal(s.host, os.hostname())
+      t.equal(s.port, 3000)
+      t.equal(s.type, 'test')
+      t.equal(s.protocol, 'tcp')
+      t.deepEqual(s.subtypes, [])
+      t.deepEqual(s.addresses, [])
+
+      if (++ups === 2) {
+        // use timeout in an attempt to make sure the invalid record doesn't
+        // bubble up
+        setTimeout(function () {
+          bonjour.destroy()
+          t.end()
+        }, 50)
+      }
+    })
+  })
+
+  bonjour.publish({ name: 'Foo Bar', type: 'test', port: 3000 }).on('up', next())
+  bonjour.publish({ name: 'Invalid', type: 'test2', port: 3000 }).on('up', next())
+  bonjour.publish({ name: 'Baz', type: 'test', port: 3000, txt: { foo: 'bar' } }).on('up', next())
+})
+
+test('bonjour.findOne - callback', function (bonjour, t) {
+  var next = afterAll(function () {
+    bonjour.findOne({ type: 'test' }, function (s) {
+      t.equal(s.name, 'Callback')
+      bonjour.destroy()
+      t.end()
+    })
+  })
+
+  bonjour.publish({ name: 'Invalid', type: 'test2', port: 3000 }).on('up', next())
+  bonjour.publish({ name: 'Callback', type: 'test', port: 3000 }).on('up', next())
+})
+
+test('bonjour.findOne - emitter', function (bonjour, t) {
+  var next = afterAll(function () {
+    var browser = bonjour.findOne({ type: 'test' })
+    browser.on('up', function (s) {
+      t.equal(s.name, 'Emitter')
+      bonjour.destroy()
+      t.end()
+    })
+  })
+
+  bonjour.publish({ name: 'Emitter', type: 'test', port: 3000 }).on('up', next())
+  bonjour.publish({ name: 'Invalid', type: 'test2', port: 3000 }).on('up', next())
+})
